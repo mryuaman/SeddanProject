a68d4bd7470f6ea6cbed230cdbb6b3dceda171ad
Yunus ACAR <yunsacr@gmail.com>
2025-03-09 21:15:26 +0300

    SeddanHussein (First Swift Tutorial)

--- SeddanProject/ViewController.swift
+++ SeddanProject/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mcqueen: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeButton(_ sender: UIButton) {
+        
+        mcqueen.image = UIImage(named: "seedan")
+    }
+    
 }
 
